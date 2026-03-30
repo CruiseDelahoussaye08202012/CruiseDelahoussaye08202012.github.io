96a581e45fe3b76719ade310c385d49abbb38a19
CruiseDelahoussaye08202012 <cpd8519974@lpssonline.com>
2026-03-30 19:02:50 +0000

    todo 8 complete

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -98,7 +98,14 @@ game.checkCirclePosition(circles[ 3 ])
 game.checkCirclePosition(circles[ 4 ])
 
             // TODO 8 / TODO 9 : Iterate over the array
-           
+            for (var i = 0; i < circles.length; i++) {
+
+               physikz.updatePosition(circles[i]);
+
+               game.checkCirclePosition(circles[i]);
+
+
+}
             
         }
     
